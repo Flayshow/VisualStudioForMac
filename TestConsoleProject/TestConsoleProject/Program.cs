3076be73dda278d30f0018a5187cf2e615d6f5cf
Flayshow <matthew.techmeier@gmail.com>
2018-01-27 21:22:04 -0500

    inital commit of new project

---
+++ TestConsoleProject/TestConsoleProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestConsoleProject
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
